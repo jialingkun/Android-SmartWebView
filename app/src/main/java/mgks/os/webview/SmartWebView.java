02fd0de746ecfa9fc43e3bb372365dedc5c545b6
Enny Hartz Tono <djaka.tingkir2@gmail.com>
2020-02-02 13:37:57 +0700

    Custom Setting
    Setup Custom setting for splash, and disabling unused feature

--- app/src/main/java/mgks/os/webview/SmartWebView.java
+++ app/src/main/java/mgks/os/webview/SmartWebView.java
@@ -16,9 +16,9 @@ class SmartWebView {
 	static boolean ASWP_CAMUPLOAD     = true;         // enable upload from camera for photos
 	static boolean ASWP_ONLYCAM       = false;        // incase you want only camera files to upload
 	static boolean ASWP_MULFILE       = true;         // upload multiple files in webview
-	static boolean ASWP_LOCATION      = true;         // track GPS locations
+	static boolean ASWP_LOCATION      = false;         // track GPS locations
 
-	static boolean ASWP_RATINGS       = true;         // show ratings dialog; auto configured ; edit method get_rating() for customizations
+	static boolean ASWP_RATINGS       = false;         // show ratings dialog; auto configured ; edit method get_rating() for customizations
 
 	static boolean ASWP_PULLFRESH     = true;         // pull refresh current url
 	static boolean ASWP_PBAR          = true;         // show progress bar in app
@@ -27,13 +27,13 @@ class SmartWebView {
 	static boolean ASWP_OFFLINE       = false;        // whether the loading webpages are offline or online
 	static boolean ASWP_EXTURL        = true;         // open external url with default browser instead of app webview
 
-	static boolean ASWP_TAB           = true;         // instead of default browser, open external URLs in chrome tab
-	static boolean ASWP_ADMOB         = true;         // to load admob or not
+	static boolean ASWP_TAB           = false;         // instead of default browser, open external URLs in chrome tab
+	static boolean ASWP_ADMOB         = false;         // to load admob or not
 
 	static boolean ASWP_EXITDIAL	  = true;		  // confirm to exit app on back press
 
 	/* -- SECURITY VARIABLES -- */
-	static boolean ASWP_CERT_VERI     = true;         // verify whether HTTPS port needs certificate verification
+	static boolean ASWP_CERT_VERI     = false;         // verify whether HTTPS port needs certificate verification
 
 
 	/* -- CONFIG VARIABLES -- */
@@ -41,7 +41,7 @@ class SmartWebView {
 	static int ASWV_LAYOUT            = 0;            // default=0; for clear fullscreen layout, and =1 for drawer layout
 
 	// URL configs
-	static String ASWV_URL            = "file:///android_asset/offline.html";     // complete URL of your website or offline webpage
+	static String ASWV_URL            = "https://www.google.com/";     // complete URL of your website or offline webpage
 	static String ASWV_SEARCH         = "https://www.google.com/search?q=";       // search query will start by the end of the present string
 	static String ASWV_SHARE_URL      = ASWV_URL + "?share=";                     // URL where you process external content shared with the app
 
